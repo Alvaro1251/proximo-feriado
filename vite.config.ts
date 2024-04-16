2900649eedff6863fa4778af2b455ea5cf1c8df0
alvaro <signorioalvaro@gmail.com>
2024-04-16 18:03:05 -0300

    deploy gh

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://Alvaro1251.github.io/proximo-feriado',
 })
